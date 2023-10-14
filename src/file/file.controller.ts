5446406598d8c0595afa1370aa4124f171e719fc
Alex <alex.gromov34502@gmail.com>
2023-10-14 16:08:57 +0300

    Реализован GET-запрос на получение картинок с сервера

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -2,11 +2,17 @@ import {
   Controller,
   HttpCode,
   Post,
+  Get,
+  Param,
+  Res,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { FileService } from './file.service';
+import * as fs from 'fs';
+import { Response } from 'express';
+import { join } from 'path';
 
 @Controller('file')
 export class FileController {
@@ -21,5 +27,20 @@ export class FileController {
     const newFile = await this.FileService.filterFile(files);
 
     return this.FileService.saveFile(newFile);
+  }
+
+  @Get(':imageName')
+  async getImage(@Param('imageName') imageName: string, @Res() res: Response) {
+    try {
+      const imagePath = join(__dirname, '..', '..', 'static', 'nft', imageName);
+      const imageStream = fs.createReadStream(imagePath);
+
+      res.setHeader('Content-Type', 'image/webp');
+
+      imageStream.pipe(res);
+    } catch (error) {
+      console.error('Ошибка при отправке изображения:', error);
+      res.status(404).send('Изображение не найдено');
+    }
   }
 }
