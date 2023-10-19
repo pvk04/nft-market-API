3c98385a5b9b5910ddd3311f0d8926b1baa9d70c
Alex <alex.gromov34502@gmail.com>
2023-10-19 13:09:27 +0300

    Добавлена CORS политика

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: true,
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTION',
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   await app.listen(4000);
 }
 bootstrap();
